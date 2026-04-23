7c27906a1d912e8c8cfb3920e252b1a49614e387
Jvjess <Jvanessa04@student.ciputra.ac.id>
2026-04-23 12:15:31 +0700

    update at Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Order extends Model
 {
@@ -17,13 +18,13 @@ class Order extends Model
         'payment_url',
         'paid_at'
     ];
-    
+
     public function user():BelongsTo    
     {
         return $this->belongsTo(User::class);
     }
 
-    public function order_details()
+    public function order_details():HasMany
     {
         return $this->hasMany(OrderDetail::class);
     }
